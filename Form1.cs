34db5953d65d2d855265abd8875fe9d8dc1e4b30
Thanh Dat Le <thanhdat.it.mmo@gmail.com>
2023-05-05 13:34:43 +0700

    5

--- Form1.cs
+++ Form1.cs
@@ -53,39 +53,97 @@ namespace export_excel
                 return;
             }
 
-            var temp = Path.GetFileNameWithoutExtension(data.MonthName);
-            var monthInStr = temp.Substring(Math.Max(0, temp.Length - 2));
-            var numNameAvailable = int.TryParse(monthInStr, out int monthInNumber);
-            var sheetNameStr = $"Tháng {monthInStr}";
-            var sheetNameNum = $"Tháng {monthInNumber}";
+            WorkBook wb = null;
+            WorkSheet ws = null;
 
-            WorkBook wb = WorkBook.Load(resultFilePathTxt.Text);
-
-            WorkSheet ws = wb.GetWorkSheet(sheetNameStr) ?? wb.GetWorkSheet(sheetNameNum);
-            if(ws == null)
+            try
             {
-                //todo
-            }
+                var temp = Path.GetFileNameWithoutExtension(data.MonthName);
+                var monthInStr = temp.Substring(Math.Max(0, temp.Length - 2));
+                var numNameAvailable = int.TryParse(monthInStr, out int monthInNumber);
+                var sheetNameStr = $"Tháng {monthInStr}";
+                var sheetNameNum = $"Tháng {monthInNumber}";
 
-            int r_index = 2;
-            var maxRow = ws.Rows.Count();
-            var needRowCount = data.Items.Sum(s => s.DsHangHoa.Count());
-            
-            if(maxRow < needRowCount)
-            {
-                var addMoreCount = needRowCount - maxRow + 5;
-                ws.InsertRows(maxRow - 1, addMoreCount);
-                var aa = ws.Rows.Count();
-                wb.Save();
-                var aaa = ws.Rows.Count();
+                wb = WorkBook.Load(resultFilePathTxt.Text);
                 ws = wb.GetWorkSheet(sheetNameStr) ?? wb.GetWorkSheet(sheetNameNum);
-            }
-            var a = ws.Rows.Count();
-            foreach (var item in data.Items)
-            {
 
+                if (ws == null)
+                {
+                    //todo
+                }
+
+                int r_index = 2;
+                var maxRow = ws.RowCount;
+                var needRowCount = data.Items.Sum(s => s.DsHangHoa.Count());
+
+                if (maxRow < needRowCount)
+                {
+                    var addMoreCount = needRowCount - maxRow + 5;
+                    for (int i = 0; i < addMoreCount; i++)
+                    {
+                        var ii = maxRow - 1 + i;
+                        ws.InsertRow(ii);
+                        ws.GetRow(ii).Value = string.Empty;
+                    }
+                    wb.Save();
+                    wb.Close();
+                    wb = WorkBook.Load(resultFilePathTxt.Text);
+                    ws = wb.GetWorkSheet(sheetNameStr) ?? wb.GetWorkSheet(sheetNameNum);
+                }
+
+                MessageBox.Show($"{ws.Name} có {data.Items.Count()} tờ khai chuẩn bị được ghi, nhấn OK để bắt đầu và kiên nhẫn chờ cho đến khi có thông báo mới, đừng tắt ứng dụng!.", "Thông báo");
+
+                int stt = 1;
+                foreach (var item in data.Items)
+                {
+                    ws.GetRow(r_index).Columns[0].FormatString = IronXL.Formatting.BuiltinFormats.Number0;
+                    ws.GetRow(r_index).Columns[0].IntValue = stt;
+
+                    ws.GetRow(r_index).Columns[1].FormatString = IronXL.Formatting.BuiltinFormats.Text;
+                    ws.GetRow(r_index).Columns[1].StringValue = item.SoToKhai;
+
+                    ws.GetRow(r_index).Columns[2].FormatString = IronXL.Formatting.BuiltinFormats.Text;
+                    ws.GetRow(r_index).Columns[2].StringValue = !string.IsNullOrWhiteSpace(item.MaNguoiNhapKhau) ? $"{item.MaNguoiNhapKhau} - {item.TenNguoiNhapKhau}" : item.TenNguoiNhapKhau;
+
+                    ws.GetRow(r_index).Columns[3].FormatString = IronXL.Formatting.BuiltinFormats.Text;
+                    ws.GetRow(r_index).Columns[3].StringValue = item.TongTrongLuongHangGross;
+
+                    ws.GetRow(r_index).Columns[4].FormatString = IronXL.Formatting.BuiltinFormats.Text;
+                    ws.GetRow(r_index).Columns[4].StringValue = item.NgayHangDiDuKien;
+
+                    ws.GetRow(r_index).Columns[5].FormatString = IronXL.Formatting.BuiltinFormats.Text;
+                    ws.GetRow(r_index).Columns[5].StringValue = item.NgayPhatHanh;
+
+                    ws.GetRow(r_index).Columns[6].FormatString = IronXL.Formatting.BuiltinFormats.Text;
+                    ws.GetRow(r_index).Columns[6].StringValue = item.TongGiaTriHoaDon;
+
+                    if (item.DsHangHoa != null && item.DsHangHoa.Any())
+                    {
+                        foreach (var sp in item.DsHangHoa)
+                        {
+                            ws.GetRow(r_index).Columns[7].FormatString = IronXL.Formatting.BuiltinFormats.Text;
+                            ws.GetRow(r_index).Columns[7].StringValue = sp.MaSoHangHoa;
+
+                            ws.GetRow(r_index).Columns[8].FormatString = IronXL.Formatting.BuiltinFormats.Text;
+                            ws.GetRow(r_index).Columns[8].StringValue = sp.MoTaHangHoa;
+                            r_index++;
+                        }
+                    }
+                    else
+                        r_index++;
+
+                    stt++;
+                }
+                wb.Save();
+                wb.Close();
+                MessageBox.Show($"{ws.Name} ghi vào hoàn tất!", "Thông báo");
             }
-            //wb.GetWorkSheet();
+            catch (Exception ex)
+            {
+                wb.Close();
+                MessageBox.Show($"{ws.Name} trạng thái ghi vào có lỗi, message: {ex.Message}. Vui lòng gửi mã lỗi này đến quản trị viên ứng dụng", "Thông báo");
+                return;
+            }
         }
     }
 }
